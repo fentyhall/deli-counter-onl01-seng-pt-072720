38d810c3d660285e540f2c8782359b460de84feb
Fenty Hall <fentymhall@gmail.com>
2020-07-30 16:38:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli === 0 
+    puts "The line is currently empty."
+  else 
+    puts "This line is currently: #{katz_deli.length}. #{katz_deli}"
+  end 
+end 
